8a27a1fdcb8ed4a1164d154e7a772e265e73604d
Iulia Soboleva <soboleva0j@gmail.com>
2023-01-11 15:06:51 +0300

    Tests for Cat, Lion and Feline classes. Jacoco report

--- src/test/java/FelineTest.java
+++ src/test/java/FelineTest.java
@@ -3,7 +3,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import java.util.List;
 
-public class FelineTests {
+public class FelineTest {
     @Test
     public void getFamilyReturnCorrectValue() {
         Feline feline = new Feline();
